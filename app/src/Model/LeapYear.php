9db5679a76ec14a1e00598108fb9d4fdd16b5270
Juan Carlos Juárez Lerma <isc.jcjl@me.com>
2023-02-01 10:43:16 -0600

    Add files for the challenge

---
+++ app/src/Model/LeapYear.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Crimsoncircle\Model;
+
+class LeapYear
+{
+    public function isLeapYear(int? $year): int
+    {
+        //TODO: Logic must be implemented to calculate if a year is a leap year
+        return 0;
+    }
+}
